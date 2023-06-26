ddff7e6bb2cfecd9987658d374407c19d32caf80
uzelkerem <uzelkerem@gmail.com>
2023-06-26 11:10:28 +0200

    update

--- 12_s00_merged.sh
+++ 12_s00_merged.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the input folder and output folder paths
-reads_dir="/mnt/groupMansuy/kerem/tasks/longrna/exp/all_preprocess_01/09_01_umidedup_alig_star_trim"
+reads_dir="/mnt/groupMansuy/kerem/tasks/longrna/exp/all_preprocess_01/09_umidedup_alig_star_trim"
 
 output_folder_main="/mnt/groupMansuy/kerem/tasks/longrna/exp/all_preprocess_01"
 output_folder0="${output_folder_main}/12_merged_featureCounts_umidedup_alig_star_trim"
@@ -20,14 +20,13 @@ num_threads=8
 
 
 # Create an array to store the filenames of all BAM files
-#bam_files=(${reads_dir}/*.bam)
+bam_files=(${reads_dir}/*.bam)
 
-# Loop through the BAM files and add them to the array
-for file in ${reads_dir}/*.bam; do
-  bam_files+=("$file")
-done
-echo $bam_files
+
+# Run featureCounts for all BAM files
+#featureCounts -T $num_threads -t exon -g gene_id -a ${genome_dir}/GCF_000001635.27_GRCm39_genomic.gtf \
+#  -o "${output_folder0}/all_counts.txt" "${bam_files[@]}" > "${log_dir0}/featureCounts.log" 2>&1
 
 # Run featureCounts for all BAM files
 featureCounts -T $num_threads -t exon -g gene_id -a ${genome_dir}/GCF_000001635.27_GRCm39_genomic.gtf \
-  -o "${output_folder0}/all_counts.txt" "${bam_files[@]}" > "${log_dir0}/featureCounts.log" 2>&1
+  -o "${output_folder0}/all_counts.txt" $(ls $reads_dir/*.bam) > "${log_dir0}/featureCounts.log" 2>&1
