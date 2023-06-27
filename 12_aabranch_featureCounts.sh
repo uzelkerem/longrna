3286e221a7efa17a04b969f0508250dadc3e6385
uzelkerem <uzelkerem@gmail.com>
2023-06-27 14:55:27 +0200

    s1 strand addition

--- 12_aabranch_featureCounts.sh
+++ 12_aabranch_featureCounts.sh
@@ -26,9 +26,13 @@ bam_files=(${reads_dir}/*.bam)
 
 
 # Run featureCounts for all BAM files
-featureCounts -T $num_threads -t exon -g gene_id -a ${genome_dir}/gencode.vM29.primary_assembly.annotation.gtf \
-  -o "${output_folder0}/aabranch_counts.txt" "${bam_files[@]}" > "${log_dir0}/aabranchfeatureCounts.log" 2>&1
+#featureCounts -T $num_threads -t exon -g gene_id -a ${genome_dir}/gencode.vM29.primary_assembly.annotation.gtf \
+#  -o "${output_folder0}/aabranch_counts.txt" "${bam_files[@]}" > "${log_dir0}/aabranchfeatureCounts.log" 2>&1
 
 # Run featureCounts for all BAM files
-featureCounts -T $num_threads -s 2 -t exon -g gene_id -a ${genome_dir}/gencode.vM29.primary_assembly.annotation.gtf \
-  -o "${output_folder0}/aabranch_counts_s02.txt" $(ls $reads_dir/*.bam) > "${log_dir0}/aabranchfeatureCounts_s02.log" 2>&1
+#featureCounts -T $num_threads -s 2 -t exon -g gene_id -a ${genome_dir}/gencode.vM29.primary_assembly.annotation.gtf \
+#  -o "${output_folder0}/aabranch_counts_s02.txt" $(ls $reads_dir/*.bam) > "${log_dir0}/aabranchfeatureCounts_s02.log" 2>&1
+
+# Run featureCounts for all BAM files
+featureCounts -T $num_threads -s 1 -t exon -g gene_id -a ${genome_dir}/gencode.vM29.primary_assembly.annotation.gtf \
+  -o "${output_folder0}/aabranch_counts_s01.txt" $(ls $reads_dir/*.bam) > "${log_dir0}/aabranchfeatureCounts_s01.log" 2>&1
