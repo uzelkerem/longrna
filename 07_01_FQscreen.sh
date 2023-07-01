4db71aba9daaf3dc3258b61465e5a69c3c8b8c5a
uzelkerem <uzelkerem@gmail.com>
2023-07-01 09:53:25 +0200

    update 07

--- 07_01_FQscreen.sh
+++ 07_01_FQscreen.sh
@@ -3,7 +3,7 @@
 # Set the input folder and output folder paths
 input_folder="/mnt/groupMansuy/kerem/tasks/longrna/exp/ee_fig3/preprocess_01/04_trimmed_data"
 output_folder="/mnt/groupMansuy/kerem/tasks/longrna/exp/ee_fig3/preprocess_01/07_01_FQscreen_trimmed_data"
-mkdir $output_folder
+mkdir -p "${output_folder}"
 
 
 # Run FastQ-screen with multiref on all files matching *trimmed.fq.gz in the input folder
