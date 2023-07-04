5003bb650bb94ecd0e6b7a1123f219221a393e14
uzelkerem <uzelkerem@gmail.com>
2023-07-04 09:17:33 +0200

    update

--- 06_02_gzipfolder.sh
+++ 06_02_gzipfolder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the input folder and output folder paths
-input_folder="/mnt/groupMansuy/kerem/tasks/longrna/exp/all_preprocess_01/06_sortmernaed_data"
+input_folder="/mnt/groupMansuy/kerem/tasks/longrna/exp/ee_fig3/preprocess_01/06_sortmernaed_data"
 output_folder="${input_folder}/zipped"
 mkdir -p "${output_folder}"
 
@@ -11,7 +11,7 @@ for file in "${input_folder}"/*other.fq; do
     mv "${file}.gz" "${output_folder}/"
 done
 
-for file in "${input_folder}"/*aligned.fq; do
-    gzip -c "$file" > "${file}.gz"
-    mv "${file}.gz" "${output_folder}/"
-done
+#for file in "${input_folder}"/*aligned.fq; do
+#    gzip -c "$file" > "${file}.gz"
+#    mv "${file}.gz" "${output_folder}/"
+#done
