8cfb99ab9d95fdf42836e1c0ac5d8756a69faf35
uzelkerem <uzelkerem@gmail.com>
2023-07-04 09:13:30 +0200

    09 01 script for fig3

---
+++ 09_fig3_umidedup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set the input folder and output folder paths
+reads_dir="/mnt/groupMansuy/kerem/tasks/longrna/exp/ee_fig3/preprocess_01/08_01_alignment_star_trim"
+output_folder="/mnt/groupMansuy/kerem/tasks/longrna/exp/ee_fig3/preprocess_01/09_01_umidedup_alig_star_trim"
+
+# Create output folder if it doesn't exist
+mkdir -p "${output_folder}"
+
+# Loop over each sample
+for file in ${reads_dir}/*.bam; do
+  # Get sample name
+  sample=$(basename "$file" .fq.gz)
+
+  #--output-stats="${output_folder}/stats"
+  # Deduplicate BAM file using umi_tools dedup
+  umi_tools dedup \
+    --stdin="${file}" \
+    --log="${output_folder}/${sample}_dedup.log" \
+    --stdout "${output_folder}/${sample}_dedup.bam" --buffer-whole-contig
+
+  # Generate index for the deduplicated BAM file
+  samtools index "${output_folder}/${sample}_dedup.bam"
+done
