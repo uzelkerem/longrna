c3c96c5c220924a97a0887e347e60fc50e479020
uzelkerem <uzelkerem@gmail.com>
2023-07-10 10:42:33 +0200

    12_02 update gtfD fig3

--- 12_02_merged_fCscript.sh
+++ 12_02_merged_fCscript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set the input folder and output folder paths
-reads_dir="/mnt/groupMansuy/kerem/tasks/longrna/exp/all_preprocess_01/09_02_umidedup_alig_star_sortmerna"
+reads_dir="/mnt/groupMansuy/kerem/tasks/longrna/exp/ee_fig3/preprocess_01/_oldbranch/09_02_umidedup_alig_star_sortmerna"
 
 
-output_folder_main="/mnt/groupMansuy/kerem/tasks/longrna/exp/all_preprocess_01/12_02_merged_featureCounts_umidedup_alig_star_sortmerna"
+output_folder_main="/mnt/groupMansuy/kerem/tasks/longrna/exp/ee_fig3/preprocess_01/_oldbranch/12_02_merged_featureCounts_umidedup_alig_star_sortmerna"
 output_folder0="${output_folder_main}/s00"
 output_folder2="${output_folder_main}/s02"
 
@@ -32,11 +32,11 @@ bam_files=(${reads_dir}/*.bam)
 
 # Run featureCounts for all BAM files
 featureCounts -T $num_threads -t exon -g gene_id -a ${genome_dir}/GCF_000001635.27_GRCm39_genomic.gtf \
-  -o "${output_folder0}/sortmerna_counts.txt" "${bam_files[@]}" > "${log_dir0}/sortmerna_featureCounts.log" 2>&1
+  -o "${output_folder0}/fig3_counts_gtfD_s00_sortmerna.txt" "${bam_files[@]}" > "${log_dir0}/fig3_counts_gtfD_s00_sortmerna.log" 2>&1
 
 # Run featureCounts for all BAM files
 featureCounts -T $num_threads -s 2 -t exon -g gene_id -a ${genome_dir}/GCF_000001635.27_GRCm39_genomic.gtf \
-  -o "${output_folder2}/sortmerna_counts_s02.txt" $(ls $reads_dir/*.bam) > "${log_dir2}/sortmerna_featureCounts_s02.log" 2>&1
+  -o "${output_folder2}/fig3_counts_gtfD_s02_sortmerna.txt" $(ls $reads_dir/*.bam) > "${log_dir2}/fig3_counts_gtfD_s02_sortmerna.log" 2>&1
 
 # Run featureCounts for all BAM files
 #featureCounts -T $num_threads -s 1 -t exon -g gene_id -a ${genome_dir}/gencode.vM29.primary_assembly.annotation.gtf \
