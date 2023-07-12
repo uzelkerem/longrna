5276e6acf24bcf38586a4779ba0c252c556c6237
uzelkerem <uzelkerem@gmail.com>
2023-07-12 12:46:01 +0200

    07 02 update

--- 07_02_FQscreen.sh
+++ 07_02_FQscreen.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Set the input folder and output folder paths
-input_folder="/mnt/groupMansuy/kerem/tasks/longrna/exp/all_preprocess_01/06_sortmernaed_data/zipped"
-output_folder="/mnt/groupMansuy/kerem/tasks/longrna/exp/all_preprocess_01/07_02_FQscreen_sortmernaed_data"
-mkdir $output_folder
+input_folder="/mnt/groupMansuy/kerem/tasks/longrna/exp/ee_fig3/preprocess_01/06_sortmernaed_data/zipped"
+output_folder="/mnt/groupMansuy/kerem/tasks/longrna/exp/ee_fig3/preprocess_01/07_02_FQscreen_sortmernaed_data"
+mkdir -p "${output_folder}"
 
 
 # Run FastQ-screen with multiref on all files matching *trimmed.fq.gz in the input folder
 for file in "${input_folder}"/*other.fq.gz; do
-  fastq_screen --aligner bowtie2 --threads 4 \
+  fastq_screen --aligner bowtie2 --threads 8 \
   --outdir $output_folder $file
 done
